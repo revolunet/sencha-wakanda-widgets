91ba1a9f4815d1d945241739a93ccac8ff0f6d96
Julien Bouquillon <julien@bouquillon.com>
2011-10-26 12:29:36 +0200

    add sample data grid widget

---
+++ Ext.grid.Panel/widget-extGridPanel.js
@@ -0,0 +1,48 @@
+/*
+Wakanda Software (the "Software") and the corresponding source code remain
+the exclusive property of 4D and/or its licensors and are protected by national
+and/or international legislations.
+This file is part of the source code of the Software provided under the relevant
+Wakanda License Agreement available on http://www.wakanda.org/license whose compliance
+constitutes a prerequisite to any use of this file and more generally of the
+Software and the corresponding source code.
+*/
+
+WAF.Widget.provide(
+
+    /**
+     *      
+     * @class TODO: give a name to this class (ex: WAF.widget.DataGrid)
+     * @extends WAF.Widget
+     */
+    'extGridPanel',   
+    {        
+        
+    },
+    /**
+     * @constructor
+     * @param {Object} inConfig configuration of the widget
+     */
+
+    /**
+     * The constructor of the widget
+     *
+     * @shared
+     * @property constructor
+     * @type Function
+     **/
+    function WAFWidget(config, data, shared) {
+        var
+        htmlObject,
+        text;
+
+        config      = config || {};
+        htmlObject  = $(this.containerNode);
+         
+    },{
+        // shared prototype
+    
+   
+    }
+);
+
